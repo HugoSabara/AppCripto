f666c50ab0370d8504dea885fb06b9dcd6928aa0
Hugo Sabara <sabarahugo@gmail.com>
2025-09-07 23:38:29 -0300

    Save project

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,81 @@
+    
+import { useState, FormEvent } from 'react'
+import { BsSearch } from 'react-icons/bs'
+import styles from './home.module.css'
+import { Link, useNavigate } from 'react-router-dom'
+
+export function Home() {
+  const [input, setInput] = useState("")
+  
+  const navigate = useNavigate();
+
+  function handleSubmit (e : FormEvent){
+    e.preventDefault();
+    if (input === "") return;
+    navigate(`/detail/${input}`);
+
+  }
+
+  function handleGetMore(){
+    alert("Mais info")
+  }
+
+  return (
+    <main className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder='Digite o nome da moeda ... EX. Bitcoin'
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type='submit'>
+          <BsSearch size={30} color='#FFF'/>
+        </button>
+      </form>  
+
+       <table>
+          <thead>
+            <tr>
+                <th scope='col'>Moeda</th>
+                <th scope='col'>Valor mercado</th>
+                <th scope='col'>Preço</th>
+                <th scope='col'>Volume</th>
+                <th scope='col'>Mudança 24h</th>
+            </tr>
+          </thead> 
+          <tbody id='tbody'>
+          
+              <tr className={styles.tr}>
+                <td className={styles.tdLabel} data-label="Moeda">
+                  <div className={styles.name}>
+                    <Link to={"/detail/bitcoin"}>
+                      <span>Bitcoin</span> | BTC
+                    </Link>
+                  </div>
+                </td>
+                <td className={styles.tdLabel} data-label="Valor mercado">
+                  1T
+                </td>
+                <td className={styles.tdLabel} data-label="Preço">
+                  8.000
+                </td>
+                <td className={styles.tdLabel} data-label="Volume">
+                  2B
+                </td>
+                <td className={styles.tdProfit} data-label="Mudança 24h">
+                  <span>1.20</span>
+                </td>
+
+              </tr>
+          
+          </tbody>
+       </table> 
+
+      <button className={styles.buttonMore} onClick={handleGetMore}>
+        Carregar Mais
+      </button>
+
+    </main>
+  )
+}
+
